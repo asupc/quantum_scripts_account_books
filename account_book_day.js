a4095dbae91d7eafa60a4119cedf9c9bfb7bbdc3
99 <99@qq.com>
2024-12-04 14:54:50 +0800

    1

--- account_book_day.js
+++ account_book_day.js
@@ -1,5 +1,10 @@
-﻿const {
-    sendNotify, addCustomData, addOrUpdateCustomDataTitle, getCustomData, deleteCustomData, sleep, updateCustomData
+﻿/**
+ * 量子变量：script_account_book_statistics_day_number ，从多少天前的数据重新统计，数字，不指定时默认60，即近60天的日记账数据会被重新统计
+ * 
+ */
+
+const {
+    sendNotify, addCustomData, getCustomData, sleep, updateCustomData
 } = require('./quantum');
 
 
@@ -10,19 +15,30 @@ const {
 const moment = require("moment")
 
 let customerDataType = "account_book_statistics_day"
+
+
+let dayNumber = 60;
+
+
 !(async () => {
-    /**
-     * 下面的-15 表示将重新计算近15天的日统计 并更行
-     */
-    let d = moment(moment().add(-15, "days").format("YYYY-MM-DD"))
+
+    try {
+        dayNumber = process.env.script_account_book_statistics_day_number ? parseInt(process.env.script_account_book_statistics_day_number) : dayNumer
+    } catch {
+    }
+
+    let d = moment(moment().add(-dayNumber, "days").format("YYYY-MM-DD"))
     let max = moment();
     for (d; d < max;) {
         let date = d.format("YYYY-MM-DD")
         let end = d.add(1, "days").format("YYYY-MM-DD")
 
-        const zhichu = (await getFlows(date, end, "支出")).reduce((accumulator, current) => {
+        const zhichuList = await getFlows(date, end, "支出");
+
+        const zhichu = zhichuList.reduce((accumulator, current) => {
             return accumulator + parseFloat(current.Data2); // 累加 Data1 的值
         }, 0).toFixed(2);
+
         const shouru = (await getFlows(date, end, "收入")).reduce((accumulator, current) => {
             return accumulator + parseFloat(current.Data2); // 累加 Data1 的值
         }, 0).toFixed(2);
@@ -46,6 +62,35 @@ let customerDataType = "account_book_statistics_day"
                 Data3: shouru,
                 Data4: (shouru - zhichu).toFixed(2)
             }])
+        }
+
+        let tagStatistics = {};
+
+        zhichuList.forEach(item => {
+            const keyValue = item.Data3;
+            if (!tagStatistics[keyValue]) {
+                tagStatistics[keyValue] = 0;
+            }
+            tagStatistics[keyValue] += parseFloat(item.Data2);;
+        });
+
+        console.log("支出标签统计：" + JSON.stringify(tagStatistics));
+
+        if (date == moment().format("YYYY-MM-DD") && (shouru > 0 || zhichu > 0)) {
+            let msg = `今日[${date}]记账统计通知`
+            if (shouru > 0) {
+                msg += `\r总收入：【${shouru}】元`
+            }
+            if (zhichu > 0) {
+                msg += `\r总支出：【${zhichu}】元`
+            }
+            if (zhichuList.length > 0) {
+                msg += `\r支出标签统计`
+                for (let k in tagStatistics) {
+                    msg += `\r${k}：【${tagStatistics[k]}】元`
+                }
+            }
+            await sendNotify(msg, true)
         }
         await sleep(1000)
     }
--- account_book_month.js
+++ account_book_month.js
@@ -1,6 +1,12 @@
-﻿const {
-    sendNotify, addCustomData, addOrUpdateCustomDataTitle, getCustomData, deleteCustomData, sleep, updateCustomData
+﻿/**
+ * 
+ * 量子变量：script_account_book_statistics_month_number，从多少月前的数据重新统计，数字，不指定时默认6，即近6个月的月记账数据会被重新统计
+ * 
+ */
+const {
+    sendNotify, addCustomData, getCustomData, sleep, updateCustomData
 } = require('./quantum');
+
 
 
 const {
@@ -10,17 +16,38 @@ const {
 const moment = require("moment")
 
 let customerDataType = "account_book_statistics_month"
+
+
+let monthNumber = 6;
+
+
 !(async () => {
-    let d = moment(moment().add(-3, "months").format("YYYY-MM-01"))
+
+
+    try {
+        monthNumber = process.env.script_account_book_statistics_month_number ? parseInt(process.env.script_account_book_statistics_month_number) : monthNumber
+    } catch {
+    }
+
+    let d = moment(moment().add(-monthNumber, "months").format("YYYY-MM-01"))
+
     let max = moment();
+
     for (d; d < max;) {
+
         let month_str = d.format("YYYY年MM月")
+
         let date = d.format("YYYY-MM-DD")
+
         let endMonthValue = moment((d.add(1, "months").valueOf() - 1)).format("YYYY-MM-DD HH:mm:ss")
 
-        const zhichu = (await getFlows(date, endMonthValue, "支出")).reduce((accumulator, current) => {
+
+        let zhichuList = await getFlows(date, endMonthValue, "支出");
+
+        const zhichu = zhichuList.reduce((accumulator, current) => {
             return accumulator + parseFloat(current.Data2); // 累加 Data1 的值
         }, 0).toFixed(2);
+
         const shouru = (await getFlows(date, endMonthValue, "收入")).reduce((accumulator, current) => {
             return accumulator + parseFloat(current.Data2); // 累加 Data1 的值
         }, 0).toFixed(2);
@@ -30,6 +57,7 @@ let customerDataType = "account_book_statistics_month"
         var cds = await getCustomData(customerDataType, null, null, {
             Data1: month_str
         });
+
         if (cds.length > 0) {
             let uData = cds[0];
             uData.Data2 = zhichu
@@ -44,6 +72,48 @@ let customerDataType = "account_book_statistics_month"
                 Data3: shouru,
                 Data4: (shouru - zhichu).toFixed(2)
             }])
+        }
+
+        let tagStatistics = {};
+
+        zhichuList.forEach(item => {
+            const keyValue = item.Data3;
+            if (!tagStatistics[keyValue]) {
+                tagStatistics[keyValue] = 0;
+            }
+            tagStatistics[keyValue] += parseFloat(item.Data2);;
+        });
+        console.log("支出标签统计：" + JSON.stringify(tagStatistics));
+
+        /**
+         * 对上一个月的数据进行统计通知
+         */
+        let tsl = [];
+
+        for (let k in tagStatistics) {
+            tsl.push({
+                t:k,
+                v:tagStatistics[k].toFixed(2)
+            })
+        }
+
+        tsl.sort((a, b) => b.v - a.v);
+        if (month_str == moment().add(-1, "months").format("YYYY年MM月")) {
+            let msg = `${month_str}记账统计通知`
+            if (shouru > 0) {
+                msg += `\r总收入：${shouru}元`
+            }
+            if (zhichu > 0) {
+                msg += `\r总支出：${zhichu}元`
+            }
+            msg+=`\r结余：${((shouru - zhichu).toFixed(2))}元`
+            if (zhichuList.length > 0) {
+                msg += `\r支出标签统计`
+                tsl.forEach((item)=>{
+                     msg += `\r${item.t}：${item.v}元`
+                })
+            }
+            await sendNotify(msg, true)
         }
         await sleep(1000)
     }
